0f13a5d1bdc17a15de65483a47373cd3df0d29d6
Maciej Urban <maciej.urban@nokia.com>
2024-02-12 14:04:03 +0100

    feat: git aliases

--- programs/git.nix
+++ programs/git.nix
@@ -5,6 +5,10 @@
     enable      = true;
     userName    = "Maciej Urban";
     userEmail   = "maciej.urban@nokia.com";
+    aliases     = {
+      mr = ''!sh -c 'git fetch $1 merge-requests/$2/head:mr-$1-$2 && git checkout mr-$1-$2' -'';
+      fetch-all = ''!sh -c 'git fetch origin "*:*"' -'';
+    };
     extraConfig = {
       core = {
         autocrif = "input";
