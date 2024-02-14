de7103f8baedcc1febd567b41fe147223ff51c46
Maciej Urban <maciej.urban@nokia.com>
2024-02-14 17:42:56 +0100

    feat(nvim): snippets

--- programs/nvim/config/lua/config/cmp.lua
+++ programs/nvim/config/lua/config/cmp.lua
@@ -9,15 +9,15 @@ if not snip_status_ok then
   return
 end
 
-vim.keymap.set({"i"}, "<C-K>", function() luasnip.expand() end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-L>", function() luasnip.jump( 1) end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-J>", function() luasnip.jump(-1) end, {silent = true})
+vim.keymap.set({ "i" }, "<C-K>", function() luasnip.expand() end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-L>", function() luasnip.jump(1) end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-J>", function() luasnip.jump(-1) end, { silent = true })
 
-vim.keymap.set({"i", "s"}, "<C-E>", function()
-	if luasnip.choice_active() then
-		luasnip.change_choice(1)
-	end
-end, {silent = true})
+vim.keymap.set({ "i", "s" }, "<C-E>", function()
+  if luasnip.choice_active() then
+    luasnip.change_choice(1)
+  end
+end, { silent = true })
 
 -- require("luasnip/loaders/from_vscode").lazy_load({ paths = { "./snippets.lua" } })
 
