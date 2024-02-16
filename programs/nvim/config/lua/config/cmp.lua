c0f3772b13f90849bff38e252760a323b0179fa5
Maciej Urban <maciej-urban@outlook.com>
2024-02-16 19:56:30 +0100

    style: plugin reformat

--- programs/nvim/config/lua/config/cmp.lua
+++ programs/nvim/config/lua/config/cmp.lua
@@ -100,43 +100,62 @@ cmp.setup({
             luasnip.lsp_expand(args.body) -- For `luasnip` users.
         end,
     },
-    ["<TAB>"] = cmp.mapping.confirm { select = true },
-  },
-  formatting = {
-    fields = { "kind", "abbr", "menu" },
-    format = function(entry, vim_item)
-      -- Kind icons
-      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-      vim_item.menu = ({
-        copilot = "[Copilot]",
-        luasnip = "[Snippet]",
-        nvim_lsp = "[LSP]",
-        buffer = "[Buffer]",
-        path = "[Path]",
-      })[entry.source.name]
-      return vim_item
-    end,
-  },
-  sources = {
-    { name = "copilot" },
-    { name = "cmdline" },
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "path" },
-    { name = "buffer" },
-  },
-  -- confirm_opts = {
-  --   behavior = cmp.ConfirmBehavior.Replace,
-  --   select = false,
-  -- },
-  -- window = {
-  --   documentation = {
-  --     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-  --   },
-  -- },
-  experimental = {
-    ghost_text = false,
-    native_menu = false,
-  },
-}
+    mapping = {
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+        ["<C-y>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+        ["<S-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+        ["<C-e>"] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        }),
+        ["<TAB>"] = cmp.mapping.confirm({ select = true }),
+    },
+    sources = {
+        { name = "copilot", priority = 3000000 },
+        { name = "luasnip", priority = 1000000 },
+        { name = "nvim_lsp", priority = 900000 },
+        { name = "cmdline", priority = 300000 },
+        { name = "async_path", priority = 100000 },
+        { name = "buffer", priority = 50000 },
+    },
+    sorting = {
+        comparators = {
+            cmp.config.compare.score,
+            cmp.config.compare.order,
+        },
+        priority_weight = 10,
+    },
+    formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+            -- Kind icons
+            vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+            -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+            vim_item.menu = ({
+                copilot = "[Copilot]",
+                luasnip = "[Snippet]",
+                nvim_lsp = "[LSP]",
+                cmdline = "[CMD]",
+                async_path = "[Path]",
+                buffer = "[Buffer]",
+            })[entry.source.name]
+            return vim_item
+        end,
+    },
+    -- confirm_opts = {
+    --   behavior = cmp.ConfirmBehavior.Replace,
+    --   select = false,
+    -- },
+    -- window = {
+    --   documentation = {
+    --     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    --   },
+    -- },
+    experimental = {
+        ghost_text = false,
+        native_menu = false,
+    },
+})
