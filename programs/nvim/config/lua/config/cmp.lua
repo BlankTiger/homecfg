f9d525ea03d4393197b4ac5ea455be9b4ef837f8
Maciej Urban <maciej.urban@nokia.com>
2024-02-16 14:27:36 +0100

    style(nvim): reformat some files

--- programs/nvim/config/lua/config/cmp.lua
+++ programs/nvim/config/lua/config/cmp.lua
@@ -1,136 +1,154 @@
 local vim = vim
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
-  return
+    return
 end
 
 local snip_status_ok, luasnip = pcall(require, "luasnip")
 if not snip_status_ok then
-  return
+    return
 end
 
-vim.keymap.set({ "i" }, "<C-K>", function() luasnip.expand() end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<C-L>", function() luasnip.jump(1) end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<C-J>", function() luasnip.jump(-1) end, { silent = true })
+vim.keymap.set({ "i" }, "<C-K>", function()
+    luasnip.expand()
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-L>", function()
+    luasnip.jump(1)
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-J>", function()
+    luasnip.jump(-1)
+end, { silent = true })
 
 vim.keymap.set({ "i", "s" }, "<C-E>", function()
-  if luasnip.choice_active() then
-    luasnip.change_choice(1)
-  end
+    if luasnip.choice_active() then
+        luasnip.change_choice(1)
+    end
 end, { silent = true })
 
 -- require("luasnip/loaders/from_vscode").lazy_load({ paths = { "./snippets.lua" } })
 
 local check_backspace = function()
-  local col = vim.fn.col "." - 1
-  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+    local col = vim.fn.col(".") - 1
+    return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
 end
 
 --   פּ ﯟ   some other good icons
 local kind_icons = {
-  Text = "",
-  Method = "m",
-  Function = "",
-  Constructor = "",
-  Field = "",
-  Variable = "",
-  Class = "",
-  Interface = "",
-  Module = "",
-  Property = "",
-  Unit = "",
-  Value = "",
-  Enum = "",
-  Keyword = "",
-  Snippet = "",
-  Color = "",
-  File = "",
-  Reference = "",
-  Folder = "",
-  EnumMember = "",
-  Constant = "",
-  Struct = "",
-  Event = "",
-  Operator = "",
-  TypeParameter = "",
+    Text = "",
+    Method = "m",
+    Function = "",
+    Constructor = "",
+    Field = "",
+    Variable = "",
+    Class = "",
+    Interface = "",
+    Module = "",
+    Property = "",
+    Unit = "",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+    Event = "",
+    Operator = "",
+    TypeParameter = "",
 }
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
-cmp.setup.cmdline('/', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
-})
-
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    {
-      name = 'cmdline',
-      option = {
-        ignore_cmds = { 'Man', '!' }
-      }
-    }
-  })
-})
-
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body) -- For `luasnip` users.
-    end,
-  },
-  mapping = {
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
-    ["<C-j>"] = cmp.mapping.select_next_item(),
-    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-    ["<C-y>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    ["<S-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ["<C-e>"] = cmp.mapping {
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
+cmp.setup.cmdline("/", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = "buffer" },
     },
-    ["<TAB>"] = cmp.mapping.confirm { select = true },
-  },
-  formatting = {
-    fields = { "kind", "abbr", "menu" },
-    format = function(entry, vim_item)
-      -- Kind icons
-      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-      vim_item.menu = ({
-        --[[ copilot = "[Copilot]", ]]
-        luasnip = "[Snippet]",
-        nvim_lsp = "[LSP]",
-        buffer = "[Buffer]",
-        path = "[Path]",
-      })[entry.source.name]
-      return vim_item
+})
+
+cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = "path" },
+    }, {
+        {
+            name = "cmdline",
+            option = {
+                ignore_cmds = { "Man", "!" },
+            },
+        },
+    }),
+})
+
+local is_whitespace = function()
+    -- returns true if the character under the cursor is whitespace.
+    local col = vim.fn.col(".") - 1
+    local line = vim.fn.getline(".")
+    local char_under_cursor = string.sub(line, col, col)
+
+    return col == 0 or string.match(char_under_cursor, "%s")
+end
+
+cmp.setup({
+    enabled = function()
+        return not is_whitespace()
     end,
-  },
-  sources = {
-    --[[ { name = "copilot" }, ]]
-    { name = "cmdline" },
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "path" },
-    { name = "buffer" },
-  },
-  -- confirm_opts = {
-  --   behavior = cmp.ConfirmBehavior.Replace,
-  --   select = false,
-  -- },
-  -- window = {
-  --   documentation = {
-  --     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-  --   },
-  -- },
-  experimental = {
-    ghost_text = false,
-    native_menu = false,
-  },
-}
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body) -- For `luasnip` users.
+        end,
+    },
+    mapping = {
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+        ["<C-y>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+        ["<S-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+        ["<C-e>"] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        }),
+        ["<TAB>"] = cmp.mapping.confirm({ select = true }),
+    },
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "cmdline" },
+        { name = "path" },
+        { name = "buffer" },
+    },
+    formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+            -- Kind icons
+            vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+            -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+            vim_item.menu = ({
+                --[[ copilot = "[Copilot]", ]]
+                luasnip = "[Snippet]",
+                nvim_lsp = "[LSP]",
+                cmdline = "[CMD]",
+                buffer = "[Buffer]",
+                path = "[Path]",
+            })[entry.source.name]
+            return vim_item
+        end,
+    },
+    -- confirm_opts = {
+    --   behavior = cmp.ConfirmBehavior.Replace,
+    --   select = false,
+    -- },
+    -- window = {
+    --   documentation = {
+    --     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    --   },
+    -- },
+    experimental = {
+        ghost_text = false,
+        native_menu = false,
+    },
+})
--- programs/nvim/config/lua/user/plugins.lua
+++ programs/nvim/config/lua/user/plugins.lua
@@ -1,408 +1,411 @@
 return {
-	"nvim-lua/plenary.nvim",
+    "nvim-lua/plenary.nvim",
 
-	"chaoren/vim-wordmotion",
+    "chaoren/vim-wordmotion",
 
-	{
-		"nvim-lualine/lualine.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("config.lualine")
-		end
-	},
+    {
+        "nvim-lualine/lualine.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("config.lualine")
+        end,
+    },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		-- lazy = true,
-		-- event = "VeryLazy",
-		config = function()
-			require("config.treesitter")
-		end,
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-			"nvim-treesitter/nvim-treesitter-context",
-			"JoosepAlviste/nvim-ts-context-commentstring",
-		}
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        -- lazy = true,
+        -- event = "VeryLazy",
+        config = function()
+            require("config.treesitter")
+        end,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            "nvim-treesitter/nvim-treesitter-context",
+            "JoosepAlviste/nvim-ts-context-commentstring",
+        },
+    },
 
-	{
-		"nvim-telescope/telescope.nvim",
-		-- event = "VeryLazy",
-		lazy = true,
-		config = function()
-			require("config.telescope")
-		end,
-		dependencies = {
-			"nvim-telescope/telescope-file-browser.nvim",
-			"junegunn/fzf",
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build =
-				"cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-			},
-			{
-				"kelly-lin/telescope-ag",
-				config = function()
-					local telescope_ag = require("telescope-ag")
-					telescope_ag.setup({
-						cmd = telescope_ag.cmds.rg, -- defaults to telescope_ag.cmds.ag
-					})
-				end
-			},
-			{
-				"paopaol/telescope-git-diffs.nvim",
-				config = function()
-					require("diffview")
-				end
-			},
-		}
-	},
+    {
+        "nvim-telescope/telescope.nvim",
+        -- event = "VeryLazy",
+        lazy = true,
+        config = function()
+            require("config.telescope")
+        end,
+        dependencies = {
+            "nvim-telescope/telescope-file-browser.nvim",
+            "junegunn/fzf",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+            },
+            {
+                "kelly-lin/telescope-ag",
+                config = function()
+                    local telescope_ag = require("telescope-ag")
+                    telescope_ag.setup({
+                        cmd = telescope_ag.cmds.rg, -- defaults to telescope_ag.cmds.ag
+                    })
+                end,
+            },
+            {
+                "paopaol/telescope-git-diffs.nvim",
+                config = function()
+                    require("diffview")
+                end,
+            },
+        },
+    },
 
-	{ "duane9/nvim-rg" },
+    { "duane9/nvim-rg" },
 
-	{
-		"folke/neodev.nvim",
-		lazy = false,
-		priority = 100,
-		config = function()
-			require("neodev").setup({})
-		end,
-		opts = {}
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		priority = 50,
-		config = function()
-			require("config.lsp")
-		end
-	},
-	{
-		"williamboman/mason.nvim",
-		lazy = true,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = true,
-	},
-	{
-		"sindrets/diffview.nvim",
-		lazy = true,
-	},
-	{
-		"nvimtools/none-ls.nvim",
-		lazy = true,
-	},
-	{
-		"MunifTanjim/prettier.nvim",
-		-- ft = { "html", "css", "js", "ts", "jsx", "tsx" },
-		lazy = true,
-		config = function()
-			require("config.prettier")
-		end
-	},
-	{ "lervag/vimtex",                 ft = "tex", lazy = true },
-	{ "barreiroleo/ltex-extra.nvim",   lazy = true },
-	{ "simrat39/rust-tools.nvim",      lazy = true },
-	{ "Vimjas/vim-python-pep8-indent", lazy = true },
+    {
+        "folke/neodev.nvim",
+        ft = "lua",
+        config = function()
+            require("neodev").setup({})
+        end,
+        opts = {},
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        priority = 50,
+        config = function()
+            require("config.lsp")
+        end,
+    },
+    {
+        "williamboman/mason.nvim",
+        lazy = true,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = true,
+    },
+    {
+        "sindrets/diffview.nvim",
+        lazy = true,
+    },
+    {
+        "nvimtools/none-ls.nvim",
+        lazy = true,
+    },
+    {
+        "MunifTanjim/prettier.nvim",
+        -- ft = { "html", "css", "js", "ts", "jsx", "tsx" },
+        lazy = true,
+        config = function()
+            require("config.prettier")
+        end,
+    },
+    { "lervag/vimtex", ft = "tex", lazy = true },
+    { "barreiroleo/ltex-extra.nvim", lazy = true },
+    { "simrat39/rust-tools.nvim", lazy = true },
+    { "Vimjas/vim-python-pep8-indent", lazy = true },
 
-	{
-		"hrsh7th/nvim-cmp",
-		event = { "InsertEnter", "CmdlineEnter" },
-		-- lazy = true,
-		config = function()
-			require("config.cmp")
-		end,
-		dependencies = {
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-cmdline",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-nvim-lua",
-			"saadparwaiz1/cmp_luasnip",
-		}
-	},
+    {
+        "hrsh7th/nvim-cmp",
+        event = { "InsertEnter", "CmdlineEnter" },
+        -- lazy = true,
+        config = function()
+            require("config.cmp")
+        end,
+        dependencies = {
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-nvim-lua",
+            "saadparwaiz1/cmp_luasnip",
+        },
+    },
 
-	-- "rafamadriz/friendly-snippets",
-	-- { "L3MON4D3/LuaSnip",               lazy = true },
-	{
-		"L3MON4D3/LuaSnip",
-		version = "v2.*",
-		config = function()
-			require("config.snippets")
-		end,
-		build = "make install_jsregexp"
-	},
+    -- "rafamadriz/friendly-snippets",
+    -- { "L3MON4D3/LuaSnip",               lazy = true },
+    {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        config = function()
+            require("config.snippets")
+        end,
+        build = "make install_jsregexp",
+    },
 
-	{
-		"lewis6991/gitsigns.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("config.gitsigns")
-		end
-	},
-	--[[ 'luk400/vim-jukit', ]]
-	{
-		"norcalli/nvim-colorizer.lua",
-		event = "VeryLazy",
-		config = function()
-			require("config.colorizer")
-		end
-	},
-	--[[ "kyazdani42/nvim-web-devicons", ]]
-	{
-		"folke/zen-mode.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("config.zen-mode")
-		end
-	},
-	{ "folke/twilight.nvim",            event = "VeryLazy" },
-	{ "romainl/vim-cool",               event = "VeryLazy" },
-	{ "ThePrimeagen/harpoon",           event = "VeryLazy" },
-	{ "ThePrimeagen/git-worktree.nvim", event = "VeryLazy" },
-	{ "mbbill/undotree",                event = "VeryLazy" },
+    {
+        "lewis6991/gitsigns.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("config.gitsigns")
+        end,
+    },
+    --[[ 'luk400/vim-jukit', ]]
+    {
+        "norcalli/nvim-colorizer.lua",
+        event = "VeryLazy",
+        config = function()
+            require("config.colorizer")
+        end,
+    },
+    --[[ "kyazdani42/nvim-web-devicons", ]]
+    {
+        "folke/zen-mode.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("config.zen-mode")
+        end,
+    },
+    { "folke/twilight.nvim", event = "VeryLazy" },
+    { "romainl/vim-cool", event = "VeryLazy" },
+    { "ThePrimeagen/harpoon", event = "VeryLazy" },
+    {
+        "ThePrimeagen/git-worktree.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("config.git-worktree")
+        end,
+    },
+    { "mbbill/undotree", event = "VeryLazy" },
 
-	{ "tpope/vim-fugitive",             event = "VeryLazy" },
-	-- { "tpope/vim-surround",             event = "VeryLazy" },
-	{ "tpope/vim-repeat",               event = "VeryLazy" },
-	{ "tpope/vim-sleuth",               event = "VeryLazy" },
-	{ "tpope/vim-obsession",            event = "VeryLazy" },
+    { "tpope/vim-fugitive", event = "VeryLazy" },
+    -- { "tpope/vim-surround",             event = "VeryLazy" },
+    { "tpope/vim-repeat", event = "VeryLazy" },
+    { "tpope/vim-sleuth", event = "VeryLazy" },
+    { "tpope/vim-obsession", event = "VeryLazy" },
 
-	--[[ "windwp/nvim-autopairs", ]]
-	--[[ { "numToStr/Comment.nvim", event = "VeryLazy" }, ]]
-	--[[ "moll/vim-bbye", ]]
-	{ "folke/which-key.nvim",           lazy = true },
-	{ "fedepujol/move.nvim",            event = "VeryLazy" },
-	--[[ "ggandor/leap.nvim", ]]
-	{ "preservim/tagbar",               event = "VeryLazy" },
-	--"TimUntersberger/neogit",
-	--[[ "amadeus/vim-evokai", ]]
-	--[[ "B4mbus/oxocarbon-lua.nvim", ]]
-	--[[ "kyazdani42/nvim-tree.lua", ]]
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		event = "VeryLazy",
-		config = function()
-			require("config.neotree")
-		end,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		}
-	},
+    --[[ "windwp/nvim-autopairs", ]]
+    --[[ { "numToStr/Comment.nvim", event = "VeryLazy" }, ]]
+    --[[ "moll/vim-bbye", ]]
+    { "folke/which-key.nvim", lazy = true },
+    { "fedepujol/move.nvim", event = "VeryLazy" },
+    --[[ "ggandor/leap.nvim", ]]
+    { "preservim/tagbar", event = "VeryLazy" },
+    --"TimUntersberger/neogit",
+    --[[ "amadeus/vim-evokai", ]]
+    --[[ "B4mbus/oxocarbon-lua.nvim", ]]
+    --[[ "kyazdani42/nvim-tree.lua", ]]
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        event = "VeryLazy",
+        config = function()
+            require("config.neotree")
+        end,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+    },
 
-	{
-		url = "https://github.com/BlankTiger/oil.nvim",
-		-- "stevearc/oil.nvim",
-		config = function()
-			require("config.oil")
-		end,
-		opts = {},
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
+    {
+        url = "https://github.com/BlankTiger/oil.nvim",
+        -- "stevearc/oil.nvim",
+        config = function()
+            require("config.oil")
+        end,
+        opts = {},
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
 
-	{
-		"glepnir/dashboard-nvim",
-		lazy = false,
-		priority = 900,
-		config = function()
-			require("config.dashboard")
-		end
-	},
+    {
+        "glepnir/dashboard-nvim",
+        lazy = false,
+        priority = 900,
+        config = function()
+            require("config.dashboard")
+        end,
+    },
 
-	--[[ { "roobert/search-replace.nvim" }, ]]
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		lazy = false,
-		priority = 900,
-		opts = {},
-	},
+    --[[ { "roobert/search-replace.nvim" }, ]]
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 900,
+        opts = {},
+    },
 
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
-	{
-		"blazkowolf/gruber-darker.nvim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"AckslD/nvim-neoclip.lua",
-		event = "VeryLazy",
-		config = function()
-			require("config.neoclip")
-		end
-	},
-	--[[ { dir = "/home/blanktiger/Projects/unorphanize.nvim" }, ]]
-	--[[ "vale1410/vim-minizinc", ]]
-	{ "NoahTheDuke/vim-just",  event = "VeryLazy" },
-	{ "kevinhwang91/nvim-bqf", event = "VeryLazy" },
-	{
-		"rcarriga/nvim-dap-ui",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			{
-				"mfussenegger/nvim-dap-python",
-				ft = "python",
-				dependencies = {
-					"mfussenegger/nvim-dap",
-					"rcarriga/nvim-dap-ui",
-				},
-				config = function(_, _)
-					local path = "~/venv/bin/python"
-					require("dap-python").setup(path)
-				end,
-			},
-		},
-		lazy = true,
-		config = function()
-			local dap = require("dap")
-			local dapui = require("dapui")
-			dapui.setup()
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited["dapui_config"] = function()
-				dapui.close()
-			end
-		end,
-		-- config = function()
-		-- end
-	},
-	-- { "chrisgrieser/nvim-spider", lazy = true },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+    {
+        "blazkowolf/gruber-darker.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "AckslD/nvim-neoclip.lua",
+        event = "VeryLazy",
+        config = function()
+            require("config.neoclip")
+        end,
+    },
+    --[[ { dir = "/home/blanktiger/Projects/unorphanize.nvim" }, ]]
+    --[[ "vale1410/vim-minizinc", ]]
+    { "NoahTheDuke/vim-just", event = "VeryLazy" },
+    { "kevinhwang91/nvim-bqf", event = "VeryLazy" },
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            {
+                "mfussenegger/nvim-dap-python",
+                ft = "python",
+                dependencies = {
+                    "mfussenegger/nvim-dap",
+                    "rcarriga/nvim-dap-ui",
+                },
+                config = function(_, _)
+                    local path = "~/venv/bin/python"
+                    require("dap-python").setup(path)
+                end,
+            },
+        },
+        lazy = true,
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
+            dapui.setup()
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close()
+            end
+        end,
+        -- config = function()
+        -- end
+    },
+    -- { "chrisgrieser/nvim-spider", lazy = true },
 
-	{
-		'echasnovski/mini.ai',
-		version = false,
-		event = "VeryLazy",
-		config = function()
-			require("config.mini-ai")
-		end
-	},
-	{
-		'echasnovski/mini.indentscope',
-		version = false,
-		event = "VeryLazy",
-		config = function()
-			require("config.mini-indentscope")
-		end
-	},
-	-- {
-	-- 	'echasnovski/mini.pairs',
-	-- 	version = false,
-	-- 	event = "VeryLazy",
-	-- 	config = function()
-	-- 		require("config.mini-pairs")
-	-- 	end
-	-- },
-	{
-		'echasnovski/mini.surround',
-		version = false,
-		event = "VeryLazy",
-		config = function()
-			require("config.mini-surround")
-		end
-	},
-	{
-		'echasnovski/mini.comment',
-		version = false,
-		event = "VeryLazy",
-		config = function()
-			require("config.mini-comment")
-		end
-	},
+    {
+        "echasnovski/mini.ai",
+        version = false,
+        event = "VeryLazy",
+        config = function()
+            require("config.mini-ai")
+        end,
+    },
+    {
+        "echasnovski/mini.indentscope",
+        version = false,
+        event = "VeryLazy",
+        config = function()
+            require("config.mini-indentscope")
+        end,
+    },
+    -- {
+    -- 	'echasnovski/mini.pairs',
+    -- 	version = false,
+    -- 	event = "VeryLazy",
+    -- 	config = function()
+    -- 		require("config.mini-pairs")
+    -- 	end
+    -- },
+    {
+        "echasnovski/mini.surround",
+        version = false,
+        event = "VeryLazy",
+        config = function()
+            require("config.mini-surround")
+        end,
+    },
+    {
+        "echasnovski/mini.comment",
+        version = false,
+        event = "VeryLazy",
+        config = function()
+            require("config.mini-comment")
+        end,
+    },
 
-	{
-		"folke/todo-comments.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("config.todo-comments")
-		end,
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
+    {
+        "folke/todo-comments.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("config.todo-comments")
+        end,
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
 
-	{
-		"folke/trouble.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("config.trouble")
-		end
-	},
+    {
+        "folke/trouble.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("config.trouble")
+        end,
+    },
 
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		opts = {
-			highlight = {
-				backdrop = true,
-				matches = true,
-				groups = {
-					match = "FlashMatch",
-					current = "FlashLabel",
-					backdrop = "FlashBackdrop",
-					label = "FlashCurrent",
-				},
-			},
-		},
-	},
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {
+            highlight = {
+                backdrop = true,
+                matches = true,
+                groups = {
+                    match = "FlashMatch",
+                    current = "FlashLabel",
+                    backdrop = "FlashBackdrop",
+                    label = "FlashCurrent",
+                },
+            },
+        },
+    },
 
-	{
-		"RRethy/vim-illuminate",
-		lazy = true,
-		-- event = "VeryLazy",
-	},
+    {
+        "RRethy/vim-illuminate",
+        lazy = true,
+        -- event = "VeryLazy",
+    },
 
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		event = "InsertEnter",
-		main = "ibl",
-		config = function()
-			require("config.indentline")
-		end
-	},
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        event = "InsertEnter",
+        main = "ibl",
+        config = function()
+            require("config.indentline")
+        end,
+    },
 
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+        },
+        config = function()
+            local noice = require("noice")
+            noice.setup()
+        end,
+        dependencies = {
+            {
+                "rcarriga/nvim-notify",
+                config = function()
+                    require("config.notify")
+                end,
+            },
+            "MunifTanjim/nui.nvim",
+        },
+    },
 
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- add any options here
-		},
-		config = function()
-			local noice = require("noice")
-			noice.setup()
-		end,
-		dependencies = {
-			{
-				"rcarriga/nvim-notify",
-				config = function()
-					require("config.notify")
-				end,
-			},
-			"MunifTanjim/nui.nvim",
-		}
-	},
+    { "nvim-pack/nvim-spectre", event = "VeryLazy" },
 
-	{ "nvim-pack/nvim-spectre",          event = "VeryLazy" },
+    { "eandrju/cellular-automaton.nvim", lazy = true },
 
-	{ 'eandrju/cellular-automaton.nvim', lazy = true },
-
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	}
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    },
 }
