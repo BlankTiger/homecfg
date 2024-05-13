7b170be38a0612fe07526055747d87636191b85f
Maciej Urban <maciej-urban@outlook.com>
2024-05-13 18:03:56 +0200

    feat(nvim): keyword highlight

--- programs/nvim/config/lua/config/settings.lua
+++ programs/nvim/config/lua/config/settings.lua
@@ -74,12 +74,14 @@ g.jupytext_style = "hydrogen"
 -- Set colorscheme
 opt.termguicolors = true
 vim.cmd.colorscheme("paragon")
+-- highlight Keyword guifg=#ffdd33
 vim.cmd([[
 highlight DiffAdd guibg=NONE guifg=#5fd7af
 highlight DiffText guibg=NONE guifg=#d7d787 
 highlight DiffDelete ctermfg=NONE ctermbg=NONE guifg=#d78787 guibg=NONE
 highlight DiffChange ctermbg=NONE guifg=NONE guibg=NONE
 highlight Visual guibg=#333333 guifg=#ffffff
+highlight Keyword guifg=#ffaf00
 
 highlight NeogitDiffAdd guibg=#1a1a1a guifg=#5fd7af
 highlight NeogitDiffAddHighlight guibg=#1a1a1a guifg=#5fd7af
