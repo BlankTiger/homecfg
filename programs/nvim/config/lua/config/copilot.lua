de5542304b4ccca0d853bd9bf989af5ae38d6335
Maciej Urban <maciej-urban@outlook.com>
2024-07-17 19:19:01 +0200

    feat(copilot): show suggestions as shadow text

--- programs/nvim/config/lua/config/copilot.lua
+++ programs/nvim/config/lua/config/copilot.lua
@@ -1,16 +1,16 @@
 local status_ok, copilot = pcall(require, "copilot")
 if not status_ok then
-  return
+    return
 end
 
 copilot.setup({
-  suggestion = { enabled = false, debounce = 0 },
-  panel = { enabled = false },
-  -- suggestion = {
-  --   auto_trigger = true,
-  --   debounce = 0,
-  --   keymap = {
-  --     accept = "<C-j>",
-  --   },
-  -- },
+    suggestion = { enabled = false, debounce = 0 },
+    panel = { enabled = false },
+    suggestion = {
+        auto_trigger = true,
+        debounce = 0,
+        keymap = {
+            accept = "<S-Tab>",
+        },
+    },
 })
