29b0e178a81add26ad754623331343874c93dfcb
Maciej Urban <maciej-urban@outlook.com>
2024-09-23 00:57:21 +0200

    huh

--- programs/nvim/config/lua/config/copilot.lua
+++ programs/nvim/config/lua/config/copilot.lua
@@ -2,7 +2,6 @@ local status_ok, copilot = pcall(require, "copilot")
 if not status_ok then
     return
 end
-
 copilot.setup({
     suggestion = { enabled = false, debounce = 0 },
     panel = { enabled = false },
