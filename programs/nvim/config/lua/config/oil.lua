edbb73244ac10018033da27fe6ebaa5a7f7cab1e
Maciej Urban <maciej-urban@outlook.com>
2024-10-11 00:18:55 +0200

    feat: show file details in oil.nvim

--- programs/nvim/config/lua/config/oil.lua
+++ programs/nvim/config/lua/config/oil.lua
@@ -3,6 +3,7 @@ if not status_ok then
     return
 end
 
+local showing_details = false
 oil.setup({
     keymaps = {
         ["<C-t>"] = false,
@@ -14,6 +15,17 @@ oil.setup({
         ["q"] = "actions.close",
         ["<leader>r"] = "actions.refresh",
         ["<BS>"] = "actions.parent",
+        ["gd"] = {
+            desc = "Toggle file detail view",
+            callback = function()
+                showing_details = not showing_details
+                if showing_details then
+                    require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+                else
+                    require("oil").set_columns({ "icon" })
+                end
+            end,
+        },
     },
     view_options = {
         show_hidden = true,
