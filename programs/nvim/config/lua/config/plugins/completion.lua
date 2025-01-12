a40d962fabe8fda80f9c84bb15389659fe93f7a3
Maciej Urban <maciej-urban@outlook.com>
2025-01-12 15:47:29 +0100

    fix: blink.cmp setup call

--- programs/nvim/config/lua/config/plugins/completion.lua
+++ programs/nvim/config/lua/config/plugins/completion.lua
@@ -17,20 +17,21 @@ return {
         ---@module 'blink.cmp'
         ---@type blink.cmp.Config
         opts = {
-            snippets = {
-                expand = function(snippet)
-                    require("luasnip").lsp_expand(snippet)
-                end,
-                active = function(filter)
-                    if filter and filter.direction then
-                        return require("luasnip").jumpable(filter.direction)
-                    end
-                    return require("luasnip").in_snippet()
-                end,
-                jump = function(direction)
-                    require("luasnip").jump(direction)
-                end,
-            },
+            snippets = { preset = "luasnip" },
+            -- snippets = {
+            --     expand = function(snippet)
+            --         require("luasnip").lsp_expand(snippet)
+            --     end,
+            --     active = function(filter)
+            --         if filter and filter.direction then
+            --             return require("luasnip").jumpable(filter.direction)
+            --         end
+            --         return require("luasnip").in_snippet()
+            --     end,
+            --     jump = function(direction)
+            --         require("luasnip").jump(direction)
+            --     end,
+            -- },
 
             keymap = { preset = "default" },
             appearance = {
@@ -41,7 +42,6 @@ return {
                 default = {
                     "lsp",
                     "path",
-                    "luasnip",
                     "snippets",
                     "buffer",
                 },
@@ -57,7 +57,7 @@ return {
                         fallbacks = {},
                         timeout_ms = 20000,
                     },
-                    luasnip = {
+                    snippets = {
                         fallbacks = {},
                         timeout_ms = 20000,
                     },
