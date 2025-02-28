62111513ca5afc741fd4dd3e03ca6318417247bd
Maciej Urban <maciej-urban@outlook.com>
2025-02-28 22:51:23 +0100

    feat: completion

--- programs/nvim/config/lua/config/plugins/completion.lua
+++ programs/nvim/config/lua/config/plugins/completion.lua
@@ -12,7 +12,7 @@ return {
                 build = "make install_jsregexp",
             },
         },
-        version = "*",
+        version = "v0.9.8",
 
         ---@module 'blink.cmp'
         ---@type blink.cmp.Config
@@ -33,7 +33,10 @@ return {
             --     end,
             -- },
 
-            keymap = { preset = "default" },
+            keymap = {
+                preset = "default",
+                ["<Tab>"] = {},
+            },
             appearance = {
                 -- use_nvim_cmp_as_default = true,
                 nerd_font_variant = "mono",
