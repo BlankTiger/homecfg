f33b1e9a11ed45c2773d8d14f67d9a8b3ce570f5
Maciej Urban <maciej-urban@outlook.com>
2025-05-03 20:07:09 +0200

    chore(nvim): harpoon cleanup

--- config/nvim/lua/config/plugins/harpoon.lua
+++ config/nvim/lua/config/plugins/harpoon.lua
@@ -4,12 +4,8 @@ return {
         branch = "harpoon2",
         config = function()
             local harpoon = require("harpoon")
-            harpoon.setup({
-                menu = {
-                    width = 120,
-                    height = 40,
-                },
-            })
+
+            harpoon.setup({})
         end,
         event = "VeryLazy",
     },
