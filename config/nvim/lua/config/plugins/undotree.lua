f644620a82258baaae90cb8f2ebabd9245efaf33
Maciej Urban <maciej-urban@outlook.com>
2025-05-03 23:19:46 +0200

    feat(nvim): undotree keymaps

--- config/nvim/lua/config/plugins/undotree.lua
+++ config/nvim/lua/config/plugins/undotree.lua
@@ -1,3 +1,9 @@
 return {
-    { "mbbill/undotree", event = "VeryLazy" },
+    {
+        "mbbill/undotree",
+        event = "VeryLazy",
+        config = function()
+            vim.keymap.set("n", "<leader>U", ":UndotreeToggle<cr>")
+        end,
+    },
 }
