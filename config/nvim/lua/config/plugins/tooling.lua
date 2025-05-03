b27716b10624e692612a1d7a71160cdc6ba17643
Maciej Urban <maciej-urban@outlook.com>
2025-05-03 23:17:56 +0200

    feat(nvim): tooling keymaps

--- config/nvim/lua/config/plugins/tooling.lua
+++ config/nvim/lua/config/plugins/tooling.lua
@@ -5,7 +5,24 @@ return {
 
     { "tpope/vim-sleuth", event = "VeryLazy" },
 
-    { "preservim/tagbar", event = "VeryLazy" },
+    {
+        "preservim/tagbar",
+        event = "VeryLazy",
+        config = function()
+            local set = vim.keymap.set
+            set("n", "<f10>", ":TagbarToggle<cr>")
+        end,
+    },
 
-    { "skywind3000/asyncrun.vim", event = "VeryLazy" },
+    {
+        "skywind3000/asyncrun.vim",
+        event = "VeryLazy",
+        config = function()
+            local set = vim.keymap.set
+
+            set({ "t", "n" }, "<F2>", function()
+                vim.cmd("copen | AsyncStop | AsyncStop | AsyncRun " .. vim.g.mk)
+            end, { silent = true })
+        end,
+    },
 }
