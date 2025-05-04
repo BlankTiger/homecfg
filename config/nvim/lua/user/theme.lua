deaa887d1baecc371c0801720f1c3c289624fb44
Maciej Urban <maciej-urban@outlook.com>
2025-05-04 17:04:47 +0200

    chore(nvim): cleanup themes.lua

--- config/nvim/lua/user/theme.lua
+++ config/nvim/lua/user/theme.lua
@@ -1,29 +1,29 @@
--- local function custom_paragon()
---     vim.cmd.colorscheme("paragon")
---     -- highlight Keyword guifg=#ffdd33
---     -- highlight Keyword guifg=#ffaf00
---     vim.cmd([[
--- highlight DiffAdd guibg=NONE guifg=#5fd7af
--- highlight DiffText guibg=NONE guifg=#d7d787
--- highlight DiffDelete ctermfg=NONE ctermbg=NONE guifg=#d78787 guibg=NONE
--- highlight DiffChange ctermbg=NONE guifg=NONE guibg=NONE
--- highlight Visual guibg=#333333 guifg=#ffffff
--- highlight Keyword guifg=#ffaf00
---
--- highlight NeogitDiffAdd guibg=#1a1a1a guifg=#5fd7af
--- highlight NeogitDiffAddHighlight guibg=#1a1a1a guifg=#5fd7af
--- highlight NeogitDiffAddCursor guibg=#0f0f0f guifg=#5fd7af
--- highlight NeogitDiffDelete ctermfg=NONE ctermbg=NONE guifg=#d78787 guibg=#1a1a1a
--- highlight NeogitDiffDeleteHighlight guifg=#d78787 guibg=#1a1a1a
--- highlight NeogitDiffDeleteCursor guibg=#0f0f0f guifg=#d78787
--- highlight! link GitConflictIncoming DiffAdd
--- highlight! link GitConflictIncomingLabel DiffAdd
--- highlight! link GitConflictCurrent DiffText
--- highlight! link GitConflictCurrentLabel DiffText
--- ]])
--- vim.cmd([[hi Comment guifg=#7FFFD4]])
--- end
---
+local custom_paragon = function()
+    vim.cmd.colorscheme("paragon")
+    -- highlight Keyword guifg=#ffdd33
+    -- highlight Keyword guifg=#ffaf00
+    vim.cmd([[
+        highlight DiffAdd guibg=NONE guifg=#5fd7af
+        highlight DiffText guibg=NONE guifg=#d7d787
+        highlight DiffDelete ctermfg=NONE ctermbg=NONE guifg=#d78787 guibg=NONE
+        highlight DiffChange ctermbg=NONE guifg=NONE guibg=NONE
+        highlight Visual guibg=#333333 guifg=#ffffff
+        highlight Keyword guifg=#ffaf00
+
+        highlight NeogitDiffAdd guibg=#1a1a1a guifg=#5fd7af
+        highlight NeogitDiffAddHighlight guibg=#1a1a1a guifg=#5fd7af
+        highlight NeogitDiffAddCursor guibg=#0f0f0f guifg=#5fd7af
+        highlight NeogitDiffDelete ctermfg=NONE ctermbg=NONE guifg=#d78787 guibg=#1a1a1a
+        highlight NeogitDiffDeleteHighlight guifg=#d78787 guibg=#1a1a1a
+        highlight NeogitDiffDeleteCursor guibg=#0f0f0f guifg=#d78787
+        highlight! link GitConflictIncoming DiffAdd
+        highlight! link GitConflictIncomingLabel DiffAdd
+        highlight! link GitConflictCurrent DiffText
+        highlight! link GitConflictCurrentLabel DiffText
+    ]])
+
+    vim.cmd([[hi Comment guifg=#7FFFD4]])
+end
 
 local black = "#000000"
 
@@ -54,5 +54,6 @@ end
 return function()
     vim.opt.termguicolors = true
     -- custom_paragon()
+    _ = custom_paragon
     custom_tokyonight()
 end
