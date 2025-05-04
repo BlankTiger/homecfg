71d592f7b5a8f55c28484e76f151a55bfa2f764e
Maciej Urban <maciej-urban@outlook.com>
2025-05-04 16:13:55 +0200

    fix(nvim): cannot have both opts and config with lazy

--- config/nvim/lua/config/plugins/completion.lua
+++ config/nvim/lua/config/plugins/completion.lua
@@ -33,130 +33,132 @@ return {
         -- version = "v0.12.4",
         version = "*",
 
-        ---@module 'blink.cmp'
-        ---@type blink.cmp.Config
-        opts = {
-            fuzzy = {
-                sorts = {
-                    function(a, b)
-                        local a_priority = source_priority[a.source_id]
-                        local b_priority = source_priority[b.source_id]
-                        if a_priority ~= b_priority then
-                            return a_priority > b_priority
-                        end
-                    end,
-                    -- defaults
-                    "score",
-                    "sort_text",
-                },
-            },
+        opts_extend = { "sources.default" },
 
-            snippets = { preset = "luasnip" },
-
-            keymap = {
-                preset = "default",
-                ["<Tab>"] = {},
-            },
-
-            appearance = {
-                nerd_font_variant = "mono",
-            },
-
-            sources = {
-                default = {
-                    "snippets",
-                    "lsp",
-                    "path",
-                    "buffer",
-                },
-                providers = {
-                    lsp = {
-                        enabled = function()
-                            return vim.g.lsp_completions_enabled
+        config = function()
+            ---@module 'blink.cmp'
+            ---@type blink.cmp.Config
+            local opts = {
+                fuzzy = {
+                    sorts = {
+                        function(a, b)
+                            local a_priority = source_priority[a.source_id]
+                            local b_priority = source_priority[b.source_id]
+                            if a_priority ~= b_priority then
+                                return a_priority > b_priority
+                            end
                         end,
-                        fallbacks = {},
-                        timeout_ms = 20000,
-                    },
-                    path = {
-                        fallbacks = {},
-                        timeout_ms = 20000,
-                    },
-                    snippets = {
-                        fallbacks = {},
-                        timeout_ms = 20000,
-                    },
-                    buffer = {
-                        -- opts = {
-                        --     get_bufnrs = function()
-                        --         -- use all buffers, even hidden ones
-                        --         -- HACK: blink.cmp by default cuts of buffer completions
-                        --         -- if buftext of all open buffers exceeds certain thresholds
-                        --         -- to get around that I edited the source code
-                        --         --
-                        --         -- there are some smart ways to solve this, some ideas:
-                        --         -- - if length is too long, include just current and alternate file
-                        --         -- - maintain a list of recently visited buffers and take N previous buffers
-                        --         --   until they don't fit anymore into the buftext
-                        --         -- - include only files of the same filetype as the current file
-                        --         -- - dont include certain filetypes
-                        --         -- - think of some way to optimize providing buftext for buffer completions
-                        --         --   currently blink.cmp loops over all files, appends their text to a list
-                        --         --   and then does table.concat on the resulting table, see if concating str
-                        --         --   could be faster without first collecting into a list, maybe nvim has a
-                        --         --   better/faster way of providing buftext for completions?
-                        --         -- - look into the algorithm for buffer completions in rust, maybe it could
-                        --         --   be parallelized further
-                        --         -- - give user more documentation on filtering out long buffers in get_bufnrs
-                        --         --   function
-                        --         return vim.api.nvim_list_bufs()
-                        --     end,
-                        -- },
-                        timeout_ms = 20000,
+                        -- defaults
+                        "score",
+                        "sort_text",
                     },
                 },
-            },
-            signature = {
-                enabled = true,
-                window = {
-                    border = "rounded",
-                    scrollbar = false,
-                    winblend = 0,
-                    winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
+
+                snippets = { preset = "luasnip" },
+
+                keymap = {
+                    preset = "default",
+                    ["<Tab>"] = {},
                 },
-            },
-            completion = {
-                list = {
-                    selection = {
-                        auto_insert = true,
+
+                appearance = {
+                    nerd_font_variant = "mono",
+                },
+
+                sources = {
+                    default = {
+                        "snippets",
+                        "lsp",
+                        "path",
+                        "buffer",
                     },
-                },
-                menu = {
-                    border = "rounded",
-                    scrollbar = false,
-                    winblend = 0,
-                    scrolloff = 2,
-                    winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
-                    draw = {
-                        gap = 2,
-                        columns = {
-                            { "label", "label_description", gap = 1 },
-                            { "kind_icon", "space" },
+                    providers = {
+                        lsp = {
+                            enabled = function()
+                                return vim.g.lsp_completions_enabled
+                            end,
+                            fallbacks = {},
+                            timeout_ms = 20000,
                         },
-                        components = {
-                            space = {
-                                ellipsis = false,
-                                text = function()
-                                    return " "
-                                end,
+                        path = {
+                            fallbacks = {},
+                            timeout_ms = 20000,
+                        },
+                        snippets = {
+                            fallbacks = {},
+                            timeout_ms = 20000,
+                        },
+                        buffer = {
+                            -- opts = {
+                            --     get_bufnrs = function()
+                            --         -- use all buffers, even hidden ones
+                            --         -- HACK: blink.cmp by default cuts of buffer completions
+                            --         -- if buftext of all open buffers exceeds certain thresholds
+                            --         -- to get around that I edited the source code
+                            --         --
+                            --         -- there are some smart ways to solve this, some ideas:
+                            --         -- - if length is too long, include just current and alternate file
+                            --         -- - maintain a list of recently visited buffers and take N previous buffers
+                            --         --   until they don't fit anymore into the buftext
+                            --         -- - include only files of the same filetype as the current file
+                            --         -- - dont include certain filetypes
+                            --         -- - think of some way to optimize providing buftext for buffer completions
+                            --         --   currently blink.cmp loops over all files, appends their text to a list
+                            --         --   and then does table.concat on the resulting table, see if concating str
+                            --         --   could be faster without first collecting into a list, maybe nvim has a
+                            --         --   better/faster way of providing buftext for completions?
+                            --         -- - look into the algorithm for buffer completions in rust, maybe it could
+                            --         --   be parallelized further
+                            --         -- - give user more documentation on filtering out long buffers in get_bufnrs
+                            --         --   function
+                            --         return vim.api.nvim_list_bufs()
+                            --     end,
+                            -- },
+                            timeout_ms = 20000,
+                        },
+                    },
+                },
+                signature = {
+                    enabled = true,
+                    window = {
+                        border = "rounded",
+                        scrollbar = false,
+                        winblend = 0,
+                        winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
+                    },
+                },
+                completion = {
+                    list = {
+                        selection = {
+                            auto_insert = true,
+                        },
+                    },
+                    menu = {
+                        border = "rounded",
+                        scrollbar = false,
+                        winblend = 0,
+                        scrolloff = 2,
+                        winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
+                        draw = {
+                            gap = 2,
+                            columns = {
+                                { "label", "label_description", gap = 1 },
+                                { "kind_icon", "space" },
+                            },
+                            components = {
+                                space = {
+                                    ellipsis = false,
+                                    text = function()
+                                        return " "
+                                    end,
+                                },
                             },
                         },
                     },
                 },
-            },
-        },
-        opts_extend = { "sources.default" },
+            }
+            require("blink.cmp").setup(opts)
 
-        config = function()
             vim.keymap.set("n", "<leader>lc", function()
                 vim.g.lsp_completions_enabled = not vim.g.lsp_completions_enabled
             end)
