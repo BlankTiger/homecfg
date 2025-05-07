3c1d61904760b1a41ec251e89bf08fcf2dc7c27d
Maciej Urban <maciej-urban@outlook.com>
2025-05-07 22:32:53 +0200

    feat(nvim): dont require saving harpoon menu buffer

--- config/nvim/lua/config/plugins/harpoon.lua
+++ config/nvim/lua/config/plugins/harpoon.lua
@@ -4,7 +4,11 @@ return {
         branch = "harpoon2",
         config = function()
             local harpoon = require("harpoon")
-            harpoon.setup({})
+            harpoon.setup({
+                settings = {
+                    save_on_toggle = true,
+                },
+            })
 
             local set = vim.keymap.set
 
