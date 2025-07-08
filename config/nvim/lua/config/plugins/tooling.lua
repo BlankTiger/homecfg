140fa85a61353a178b6d00567557e58a56208577
Maciej Urban <maciej-urban@outlook.com>
2025-07-08 22:36:24 +0200

    feat(nvim): test out autosave

--- config/nvim/lua/config/plugins/tooling.lua
+++ config/nvim/lua/config/plugins/tooling.lua
@@ -40,4 +40,12 @@ return {
             })
         end,
     },
+
+    {
+        "okuuva/auto-save.nvim",
+        version = "^1.0.0",
+        cmd = "ASToggle",
+        event = { "InsertLeave", "TextChanged" },
+        opts = {},
+    },
 }
