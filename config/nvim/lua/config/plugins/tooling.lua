f2ebe2fdfcf78074131b798b7fab415f1c494924
Maciej Urban <maciej-urban@outlook.com>
2025-09-25 21:12:05 +0200

    feat(nvim-tooling): overseer for task running

--- config/nvim/lua/config/plugins/tooling.lua
+++ config/nvim/lua/config/plugins/tooling.lua
@@ -1,11 +1,50 @@
 local set = vim.keymap.set
 
+local function run_new_task_from_mk(o)
+    local task = o.new_task({
+        cmd = vim.g.mk
+    })
+    task:start()
+end
+
 set("n", "<F10>", "<cmd>TagbarToggle<cr>", vim.g.n_opts)
-set({ "t", "n" }, "<F1>", "<cmd>AsyncStop<cr>", vim.g.n_opts)
+
+set({ "t", "n" }, "<F1>", function()
+    local o = require("overseer")
+
+    local all_tasks = o.list_tasks({})
+    local len_tasks = #all_tasks
+    if len_tasks == 0 then
+        return
+    end
+
+    local last_task = all_tasks[len_tasks]
+    last_task:stop()
+end, vim.g.n_opts)
+
 set({ "t", "n" }, "<F2>", function()
-    vim.cmd("AsyncStop")
-    vim.cmd("copen | AsyncRun " .. vim.g.mk)
-    vim.cmd("wincmd p")
+    local o = require("overseer")
+    local s = require("overseer.constants").STATUS
+    -- o.open({ enter = false })
+
+    local all_tasks = o.list_tasks({})
+    local len_tasks = #all_tasks
+    if len_tasks == 0 then
+        run_new_task_from_mk(o)
+        return
+    end
+
+    local last_task = all_tasks[len_tasks]
+    if last_task.cmd == vim.g.mk then
+        if last_task.status == s.RUNNING then
+            last_task:stop()
+        end
+
+        last_task:restart()
+        return
+    end
+
+    run_new_task_from_mk(o)
 end, vim.g.n_opts)
 -- F26 = C-F2
 set({ "t", "n" }, "<F26>", function()
@@ -56,9 +95,56 @@ return {
         cmd = { "TagbarToggle" },
     },
 
+    -- {
+    --     "skywind3000/asyncrun.vim",
+    --     cmd = { "AsyncRun", "AsyncStop" },
+    -- },
+
+    -- "tpope/vim-dispatch",
+
     {
-        "skywind3000/asyncrun.vim",
-        cmd = { "AsyncRun", "AsyncStop" },
+        "blanktiger/overseer.nvim",
+        event = "VeryLazy",
+        opts = {
+            task_list = {
+                direction = "right",
+                bindings = {
+                    ["?"] = "ShowHelp",
+                    ["g?"] = "ShowHelp",
+                    ["<CR>"] = "RunAction",
+                    ["<C-r>"] = "Restart",
+                    ["<C-e>"] = "Edit",
+                    ["o"] = "Open",
+                    ["<C-v>"] = "OpenVsplit",
+                    ["<C-s>"] = "OpenSplit",
+                    ["<C-n>"] = "OpenTab",
+                    ["<C-q>"] = false,
+                    ["p"] = "TogglePreview",
+                    ["<C-l>"] = false,
+                    ["<C-h>"] = false,
+                    ["<C-k>"] = false,
+                    ["<C-j>"] = false,
+                    ["R"] = "Restart",
+                    ["K"] = "Stop",
+                    ["L"] = "IncreaseDetail",
+                    ["H"] = "DecreaseDetail",
+                    ["["] = "DecreaseWidth",
+                    ["]"] = "IncreaseWidth",
+                    ["{"] = "PrevTask",
+                    ["}"] = "NextTask",
+                    ["q"] = "Close",
+                },
+            },
+            component_aliases = {
+                -- Most tasks are initialized with the default components
+                default = {
+                    { "display_duration", detail_level = 2 },
+                    "on_output_summarize",
+                    "on_exit_set_status",
+                    { "on_output_quickfix", open = true },
+                },
+            },
+        },
     },
 
     {
