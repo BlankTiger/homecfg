39ea8b976dfc85821a4eb53f014056435b091420
Maciej Urban <maciej-urban@outlook.com>
2025-10-24 18:46:06 +0200

    feat(nvim): some bindings for aqf

--- config/nvim/lua/config/plugins/quickfix.lua
+++ config/nvim/lua/config/plugins/quickfix.lua
@@ -4,11 +4,17 @@ return {
         dev = true,
         lazy = true,
         config = function()
-            require("aqf").setup({
-                show_instructions = false,
+            local aqf = require("aqf")
+            aqf.setup({
+                show_instructions = true,
                 windowed = false,
                 debug = false,
             })
+
+            local set = vim.keymap.set
+            set("n", "<space>E", aqf.edit_curr_qf, { desc = "Edit current quickfix list" })
+            set("n", "<space>H", aqf.show_saved_qf_lists, { desc = "Show saved quickfix lists" })
+            set("n", "<space>S", aqf.save_qf, { desc = "Save current quickfix to history" })
         end,
     },
 }
