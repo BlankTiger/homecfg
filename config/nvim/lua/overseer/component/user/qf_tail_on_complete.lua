90bf6c5ff966d0c4d4db46d71d8fcc54ae511487
Maciej Urban <maciej-urban@outlook.com>
2026-01-06 20:02:05 +0100

    feat(nvim): remove trailing empty lines when overseer task is finished

--- config/nvim/lua/overseer/component/user/qf_tail_on_complete.lua
+++ config/nvim/lua/overseer/component/user/qf_tail_on_complete.lua
@@ -5,15 +5,27 @@ return {
         return {
             on_complete = function()
                 vim.schedule(function()
-                    local qf = vim.fn.getqflist({ winid = 0 })
+                    local qf = vim.fn.getqflist({ winid = 0, items = 1 })
                     local winid = qf.winid or 0
                     if winid == 0 then
                         return
                     end
 
-                    local bufnr = vim.api.nvim_win_get_buf(winid)
-                    local last_line = vim.api.nvim_buf_line_count(bufnr)
-                    vim.api.nvim_win_set_cursor(winid, { last_line, 0 })
+                    local items = qf.items
+                    local last_valid = #items
+
+                    while last_valid > 0 and items[last_valid].text:match("^%s*$") do
+                        last_valid = last_valid - 1
+                    end
+
+                    if last_valid < #items then
+                        vim.fn.setqflist({}, "r", { items = vim.list_slice(items, 1, last_valid) })
+                    end
+
+                    if last_valid > 0 then
+                        local bufnr = vim.api.nvim_win_get_buf(winid)
+                        vim.api.nvim_win_set_cursor(winid, { last_valid, 0 })
+                    end
                 end)
             end,
         }
